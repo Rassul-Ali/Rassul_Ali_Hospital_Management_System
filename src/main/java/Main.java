a8e8a30144f0a50c498e8b3a90acef0f8d83e612
Rassul Ali <alirassul78@gmail.com>
2025-04-24 21:35:51 +0200

    Criando artefacto do situacao corrente

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
-//public class Main{
-    //public static void main(String[] args) {
-        //com.javafx.hospital.HospitalApplication.main(args);
-    //}
-//}
+public class Main{
+    public static void main(String[] args) {
+        com.javafx.hospital.HospitalApplication.main(args);
+    }
+}
